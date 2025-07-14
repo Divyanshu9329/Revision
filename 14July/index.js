0fbf699812adf3d989078dafe8256f979e430a30
Divyanshu Pandey <divyanshupandey9329@gmail.com>
2025-07-14 12:02:27 +0530

    Blocking Code

---
+++ 14July/index.js
@@ -0,0 +1,16 @@
+function add(a,b){
+    var c = a+b;
+    console.log("Add :",c)
+}
+
+// setTimeout(() => {
+//     add(32,42);
+// }, 3000);
+// or
+setTimeout(add, 2000, 32,12);
+
+
+console.log("first");
+
+console.log("Ending");
+console.log("Thanks");
