e4e6a532e0c81368826905fb2fe0ad3f50bbf9d5
Divyanshu Pandey <divyanshupandey9329@gmail.com>
2025-07-30 14:11:21 +0530

    React.....

--- 29July/src/App.jsx
+++ 29July/src/App.jsx
@@ -10,19 +10,43 @@
 
 // import Demo from './components/Demo'
 
+import React, { useState } from 'react'
+
 import Comp1 from "./components/Comp1"
 
 export default function App(){
+
+  const [index, setindex] = useState(0)
+
+  var DN = [
+    {
+      color : "white",
+      backgroundColor : "Black"
+    },
+    {
+      color : "Black",
+      backgroundColor : "white"
+    }
+  ]
   return(
-    <>
+    <div style={DN[index]}>
     <Comp1/>
+    <br />
+    <button onClick={
+      ()=>{
+      if(index==0) 
+        setindex(1)
+      else{
+        setindex(0);
+      }}
+    }>Day/Night</button>
     {/* <h2>
       Hello sir
     </h2>
     <Try/>
     <Second/>
     <Demo/> */}
-    </>
+    </div>
   )
 }
 
